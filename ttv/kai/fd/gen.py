24501d45c92ec50e121f48b3b463ccfe561b6e53
Moonshot <moonshot11@users.noreply.github.com>
2023-08-17 20:57:53 -0700

    Add grouped food files

--- ttv/kai/fd/gen.py
+++ ttv/kai/fd/gen.py
@@ -4,6 +4,7 @@
 # Generate food
 #
 
+import itertools
 import random
 
 words = {}
@@ -25,15 +26,23 @@ def init_data():
 def pick(key):
     return random.choice(words[key])
 
-def gen_files():
-    for key in words:
-        for i, term in enumerate(words[key]):
-            with open(f"{key}_{i}.fd", "w") as fh:
-                fh.write(term)
+def gen_file(keys, filename, joiner=' '):
+    idx = 0
+    valid = [v for k,v in words.items() if k in keys]
+    valid.reverse()
+    print(valid)
+    for items in itertools.product(*valid):
+        buf = joiner.join(items)
+        with open(f"{filename}{idx}.fd", "w") as fh:
+            fh.write(buf)
+        idx += 1
 
 if __name__ == "__main__":
     init_data()
-    gen_files()
-    buf = f"USER prepares a {pick('app_type')} {pick('app_base')} for USER2, followed by a main course of {pick('main_cook')} {pick('main_food')} in {pick('main_add')} with {pick('main_amt')} of {pick('main_add')}. For dessert, USER prepared a {pick('des_flavor')} {pick('des_food')}. Yum!"
+    gen_file( ["app_type", "app_base"], "app" )
+    gen_file( ["des_flavor", "des_food"], "des" )
+    gen_file( ["main_cook", "main_food"], "mainA" )
+    gen_file( ["main_amt", "main_add"], "mainB", joiner=" of " )
 
-    print(buf)
+    buf = f"USER prepares a {pick('app_type')} {pick('app_base')} for USER2, followed by a main course of {pick('main_cook')} {pick('main_food')} in {pick('main_add')} with {pick('main_amt')} of {pick('main_add')}. For dessert, USER prepared a {pick('des_flavor')} {pick('des_food')}. Yum!"
+    #print(buf)
