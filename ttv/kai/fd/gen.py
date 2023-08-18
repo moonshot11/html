5c3606d685e7dfda83ddb4952f0011510cd9b4ca
Moonshot <moonshot11@users.noreply.github.com>
2023-08-17 21:33:55 -0700

    Minor tweaks

--- ttv/kai/fd/gen.py
+++ ttv/kai/fd/gen.py
@@ -29,19 +29,18 @@ def pick(key):
 def gen_file(keys, filename, joiner=' '):
     idx = 0
     valid = [words[k] for k in keys]
-    print(valid)
+
     for items in itertools.product(*valid):
         buf = joiner.join(items)
         with open(f"{filename}{idx}.fd", "w") as fh:
             fh.write(buf)
         idx += 1
 
+    print(f"{filename}: {idx-1}")
+
 if __name__ == "__main__":
     init_data()
     gen_file( ["app_type", "app_base"], "app" )
-    gen_file( ["des_flavor", "des_food"], "des" )
     gen_file( ["main_cook", "main_food"], "mainA" )
     gen_file( ["main_amt", "main_add"], "mainB", joiner=" of " )
-
-    buf = f"USER prepares a {pick('app_type')} {pick('app_base')} for USER2, followed by a main course of {pick('main_cook')} {pick('main_food')} in {pick('main_add')} with {pick('main_amt')} of {pick('main_add')}. For dessert, USER prepared a {pick('des_flavor')} {pick('des_food')}. Yum!"
-    #print(buf)
+    gen_file( ["des_flavor", "des_food"], "des" )
