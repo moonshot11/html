ad580e3c830f6d6175ee762ef9ccd999c319e379
Moonshot <moonshot11@users.noreply.github.com>
2023-10-06 09:30:52 -0700

    Add spoopy entries

--- ttv/kai/gen_animals.py
+++ ttv/kai/gen_animals.py
@@ -1,16 +1,20 @@
 #!python
 
+import sys
+
 #
 # gen_animals.py
 #
 
 count = 0
+filename = sys.argv[1] if len(sys.argv) >= 2 else "animals.txt"
+destpath = sys.argv[2] if len(sys.argv) >= 3 else "animals"
 
-with open("animals.txt", "rb") as fin:
+with open(filename, "rb") as fin:
     for line in fin.readlines():
         line = line.strip()
         if not line:
             continue
-        with open(f"animals/a{count}.txt", "wb") as fout:
+        with open(f"{destpath}/a{count}.txt", "wb") as fout:
             fout.write(line)
         count += 1
